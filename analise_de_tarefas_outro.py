cc17b3d5307a62415b4b094640721ad2a8319f6e
lucianohcl <lucianonetexame21@gmail.com>
2026-04-19 21:10:48 -0300

    Update: Ajustes finais na interface de login e banner

--- analise_de_tarefas_outro.py
+++ analise_de_tarefas_outro.py
@@ -2264,7 +2264,7 @@ import streamlit as st
 def salvar_no_github(conteudo_dict, nome_arquivo):
     try:
         g = Github(st.secrets["DB_TOKEN"])
-        repo = g.get_repo("lucianohcl/formulario-novo")
+        repo = g.get_repo("lucianohcl/formulario-colaborador")
         caminho_git = f"dados/{nome_arquivo}"
         
         json_string = json.dumps(conteudo_dict, ensure_ascii=False, indent=4)
@@ -2468,7 +2468,7 @@ g = Github(TOKEN)
 
 # ESSE É O PONTO: Coloque o texto direto aqui para não dar erro de "REPO_NOME not defined"
 
-repo = g.get_repo("lucianohcl/formulario-novo") 
+repo = g.get_repo("lucianohcl/formulario-colaborador") 
 
 
 
